9229ceb1b185c59d0dd87ef5c03a90a4d0846808
briankawahala <briankawahala@hotmail.com>
2024-04-27 16:45:55 -0300

    passando alteracoes

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/projetos")
 def hello_world():
     return "<p>Hello, World!</p>"
+
+@app.route("/Prazo")
+def Prazsdfo():
+    return render_template('landingpage.html')
     
 if __name__ == '__main__':
     app.run()
